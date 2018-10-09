280c821498e5dd6f9826f65495cfc1fcc01a508c
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2018-10-09 16:39:07 +0200

    viser tilfældigt roterede billeder på forsiden

--- xml-seek-word.js
+++ xml-seek-word.js
@@ -1,8 +1,64 @@
 /**
  * file: xml-seek-word.html
  * formål: henter data om bøger fra EAAA Bibliotekets API
+
+ Denne let reviderede version viser
+ en række forsider ved et AJ
+
  **/
 $(document).ready(function() {
+
+  // "renser" #indhold ...
+  $('#indhold').html('');
+  //alert('visForsider() klar');
+
+  // vider forsider af nyheder ved load
+  let visForsider = function(){
+
+    // API url: list all news
+    let newsListAll = 'https://eaaa.reindex.net/EAAA/main/Api.php?Focus=newslistall';
+
+      // GET AJAX the Jquery way
+      $.ajax({
+        type: "GET",
+        url: newsListAll,
+        cache: false,
+        dataType: "xml",
+        success: function(xml) {
+
+          $(xml).find('item').each(function() {
+
+            let billede = $(this).find('enclosure').attr("url"); // src til billedet
+            let permalink = $(this).find('guid').text(); // returnerer streng med mellemrum
+            permalink = permalink.replace(/\s/g, ''); // fjerner mellemrum m. regexp
+
+            let nytBillede = function(perm, img) {
+
+              billedLink = '<a href="' + perm + '" target="_blank">' +
+                '<img src="' +
+                img +
+                '" alt="billede" class="enForside" style="max-width:200px;transform: rotate('+
+                Math.floor(Math.random()*85) +
+                'deg);">' +
+                '</a>';
+
+              if (billede !== undefined) {
+                $('#indhold').append(billedLink);
+              }
+              else {
+                $('#indhold').append('<!-- billede mangler -->')
+              }
+            } // nytBillede slut
+
+            nytBillede(permalink, billede);
+
+          })
+        }
+      }); // ajax slut
+  }
+
+  // kalder vis visForsider
+  visForsider();
 
   // get user input
   $("#myTextBox").on("change paste keyup", function() {
