3dae159d10df82bb99d9879ff786c1aa0a9afe4f
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2018-10-09 16:50:35 +0200

    beautify
    og slut for i dag

--- xml-seek-word.js
+++ xml-seek-word.js
@@ -14,48 +14,47 @@ $(document).ready(function() {
   //alert('visForsider() klar');
 
   // vider forsider af nyheder ved load
-  let visForsider = function(){
+  let visForsider = function() {
 
     // API url: list all news
     let newsListAll = 'https://eaaa.reindex.net/EAAA/main/Api.php?Focus=newslistall';
 
-      // GET AJAX the Jquery way
-      $.ajax({
-        type: "GET",
-        url: newsListAll,
-        cache: false,
-        dataType: "xml",
-        success: function(xml) {
+    // GET AJAX the Jquery way
+    $.ajax({
+      type: "GET",
+      url: newsListAll,
+      cache: false,
+      dataType: "xml",
+      success: function(xml) {
 
-          $(xml).find('item').each(function() {
+        $(xml).find('item').each(function() {
 
-            let billede = $(this).find('enclosure').attr("url"); // src til billedet
-            let permalink = $(this).find('guid').text(); // returnerer streng med mellemrum
-            permalink = permalink.replace(/\s/g, ''); // fjerner mellemrum m. regexp
+          let billede = $(this).find('enclosure').attr("url"); // src til billedet
+          let permalink = $(this).find('guid').text(); // returnerer streng med mellemrum
+          permalink = permalink.replace(/\s/g, ''); // fjerner mellemrum m. regexp
 
-            let nytBillede = function(perm, img) {
+          let nytBillede = function(perm, img) {
 
-              billedLink = '<a href="' + perm + '" target="_blank">' +
-                '<img src="' +
-                img +
-                '" alt="billede" class="enForside" style="max-width:200px;transform: rotate('+
-                Math.floor(Math.random()*85) +
-                'deg);">' +
-                '</a>';
+            billedLink = '<a href="' + perm + '" target="_blank">' +
+              '<img src="' +
+              img +
+              '" alt="billede" class="enForside" style="max-width:200px;transform: rotate(' +
+              Math.floor(Math.random() * 85) +
+              'deg);">' +
+              '</a>';
 
-              if (billede !== undefined) {
-                $('#indhold').append(billedLink);
-              }
-              else {
-                $('#indhold').append('<!-- billede mangler -->')
-              }
-            } // nytBillede slut
+            if (billede !== undefined) {
+              $('#indhold').append(billedLink);
+            } else {
+              $('#indhold').append('<!-- billede mangler -->')
+            }
+          } // nytBillede slut
 
-            nytBillede(permalink, billede);
+          nytBillede(permalink, billede);
 
-          })
-        }
-      }); // ajax slut
+        })
+      }
+    }); // ajax slut
   }
 
   // kalder vis visForsider
@@ -104,24 +103,24 @@ $(document).ready(function() {
           let pubdate = $(this).find('pubDate').text();
 
           // er der et billede? J/N?
-          function visBillede(img){
-            if (img !== undefined){ // hvis img ikke er undefined
+          function visBillede(img) {
+            if (img !== undefined) { // hvis img ikke er undefined
               return '<img class="center" src="' + img + '" alt="billede af bogen">';
             }
-            if (img == undefined){ // hvis billede er undefined
+            if (img == undefined) { // hvis billede er undefined
               return '<!-- billede af forsiden mangler -->';
             }
           }
 
           // tilføjer (append) markup til #indhold
           $('#indhold').append('<div class="bog">' + // .bog en bogkasse begynder
-            '<a href="' + permalink + '" target="_blank">'+ visBillede( billede ) + '</a>' + // billede (måske)
+            '<a href="' + permalink + '" target="_blank">' + visBillede(billede) + '</a>' + // billede (måske)
             '<h3><i class="fas fa-book"></i> ' + titel + '</h3>' + // titel
             '<h4><i class="fas fa-user-circle"></i> By: ' + forfatter + '</h4>' + // forfatter
             // visBillede( billede ) + // skriver kun billedtag, hvis der er et billede
             '<p>' + beskrivelse + '</p>' + // beskrivelse
-            '<p>Published: ' + pubdate.substring(0,16) + '</p>' + // tryk: dato
-            '<a href="' + permalink + '" target="_blank">'+ '<i class="fas fa-location-arrow"></i> Bestil </a>' + // permalink
+            '<p>Published: ' + pubdate.substring(0, 16) + '</p>' + // tryk: dato
+            '<a href="' + permalink + '" target="_blank">' + '<i class="fas fa-location-arrow"></i> Bestil </a>' + // permalink
             '</div>' // bogkasse slut
           );
         })
